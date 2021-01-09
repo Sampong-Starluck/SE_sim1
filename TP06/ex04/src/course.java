f33aca987658490a200a57cf5d5434165d5d09da
sampong89 <samponglim17@gmail.com>
2021-01-09 13:41:05 +0700

    ex04

---
+++ TP06/ex04/src/course.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class course {
+    String course;
+    ArrayList<course> list = new ArrayList<>();
+
+    public course(String course) {
+        this.course = course;
+    }
+    
+}
+
+class driver {
+    public static void main(String[] args) {
+
+    }
+}
