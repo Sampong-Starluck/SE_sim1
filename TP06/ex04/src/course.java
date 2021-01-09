9a4648c781d40fae55eda1cc6bc8d61db94af936
sampong89 <samponglim17@gmail.com>
2021-01-09 14:56:51 +0700

    finish

--- TP06/ex04/src/course.java
+++ TP06/ex04/src/course.java
@@ -1,17 +1,48 @@
 import java.util.*;
 
 public class course {
-    String course;
-    ArrayList<course> list = new ArrayList<>();
+    private static Scanner sc;
 
-    public course(String course) {
-        this.course = course;
-    }
-    
-}
-
-class driver {
     public static void main(String[] args) {
-
+        ArrayList<String> courses = new ArrayList<>();
+        sc = new Scanner(System.in);
+        // let's add 5 course as a base course.
+        courses.add("Database");
+        courses.add("Software Engineering");
+        courses.add("Network");
+        courses.add("Distributed System");
+        courses.add("Internet Programming");
+        while (true) {
+            System.out.println();
+            System.out.println("1. List all courses.");
+            System.out.println("2. Find courses by name.");
+            System.out.println("3. Add new course.");
+            System.out.println("4. Quit");
+            System.out.print("Enter your option: ");
+            int key = sc.nextInt();
+            switch (key) {
+                case 1 -> {
+                    for (String i : courses) {
+                        System.out.println(i);
+                    }
+                }
+                case 2 -> {
+                    System.out.print("Enter the course's name: ");
+                    String course = sc.next();
+                    for (String i : courses) {
+                        if (i.equals(course)) {
+                            System.out.print(i);
+                        }
+                    }
+                }
+                case 3 -> {
+                    System.out.print("Enter the new course's name: ");
+                    String newCourse = sc.next();
+                    courses.add(newCourse);
+                }
+                case 4 -> System.exit(0);
+                default -> System.err.println("Your input is incorrect !!!!!!");
+            }
+        }
     }
 }
